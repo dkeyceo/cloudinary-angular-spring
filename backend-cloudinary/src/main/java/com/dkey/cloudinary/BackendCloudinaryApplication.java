6411115e3e9407912fc6e28b982f10a3d432eda4
dkeyceo <dkeyceo@gmail.com>
2023-07-28 22:13:46 +0300

    Generated empty backend

---
+++ backend-cloudinary/src/main/java/com/dkey/cloudinary/BackendCloudinaryApplication.java
@@ -0,0 +1,13 @@
+package com.dkey.cloudinary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendCloudinaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendCloudinaryApplication.class, args);
+	}
+
+}
---
+++ backend-cloudinary/src/test/java/com/dkey/cloudinary/BackendCloudinaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dkey.cloudinary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendCloudinaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
